a70aea4f5fb8c91d8e3f38050d9313895f5f037d
Nestor Malynovskyi <nestor7855@gmail.com>
2020-10-28 01:44:04 +0200

    Initial Commit

---
+++ mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mobile
+//
+//  Created by Nestor on 10/28/20.
+//  Copyright © 2020 Nestor Malynovskyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
